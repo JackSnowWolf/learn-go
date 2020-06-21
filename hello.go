9b23954d66ebb59d9041e7f68fe1f6e4a0d55098
huchong <1178869226@qq.com>
2020-06-21 18:30:33 -0400

    [REVISE] hello.go

--- hello.go
+++ hello.go
@@ -11,8 +11,25 @@ func main() {
 	var str string = fmt.Sprintf("%s%d%s", "Alice is ", 10, " years old.")
 	fmt.Println(str)
 
-	b, c := 5, 7
-	fmt.Println(b, c)
+	var a int = 21
+	var b int = 10
+	var c int
+
+	c = a + b
+	fmt.Printf("1st line c value: %d\n", c)
+	c = a - b
+	fmt.Printf("2nd line c value: %d\n", c)
+	c = a * b
+	fmt.Printf("3rd line c value:%d\n", c)
+	c = a / b
+	fmt.Printf("4th line c value: %d\n", c)
+	c = a % b
+	fmt.Printf("5th line c value: %d\n", c)
+	a++
+	fmt.Printf("6th line c value: %d\n", a)
+	a = 21
+	a--
+	fmt.Printf("7th line c value: %d\n", a)
 
 	tag = false
 	fmt.Println(tag)
